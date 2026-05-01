6ba842844792923d05f85622ad4666a9975e00dc
zhangliangr <zhangliangr@chanjet.com>
2026-05-01 10:02:35 +0800

    SYKFPT-1093 Export ConnectionState from SDK lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod protocol;
 pub mod crypto;
 pub mod dispatcher;
 
-pub use client::{GatewayClient, ClientOptions};
+pub use client::{GatewayClient, ClientOptions, ConnectionState};
 pub use protocol::{
     EventFrame, AckFrame, BaseMessage, AppTicketMessage, 
     EntAuthCodeMessage, OrderStatusMessage, AppNoticeMessage
